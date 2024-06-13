b92aa5639c49db24e9c7f644f19450eb05e8ba48
Lx5669 <13923114+lx5669@user.noreply.gitee.com>
2024-06-13 19:42:04 +0800

    chore:登录管理补充

--- lease/web/web-app/src/main/java/com/cwnu/lease/web/app/service/LoginService.java
+++ lease/web/web-app/src/main/java/com/cwnu/lease/web/app/service/LoginService.java
@@ -7,6 +7,8 @@ import org.apache.ibatis.annotations.Mapper;
 public interface LoginService {
     void getCode(String phone);
 
+    UserInfoVo getUserInfoId(Long id);
+
     String login(LoginVo loginVo);
 
     UserInfoVo getLoginUserById(Long userId);
