c601c3f5d46bc6025995ac8a671a7e02fb0db74a
Jisam <353753088@qq.com>
2025-08-13 21:39:35 +0800

    合并

--- lease/common/src/main/java/com/cwnu/lease/common/minio/Minioconfiguration.java
+++ lease/common/src/main/java/com/cwnu/lease/common/minio/Minioconfiguration.java
@@ -7,6 +7,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * MinIO配置类。
+ * 该类配置了MinIO客户端的bean，并基于MinIO服务端的属性进行初始化。
+ * @author Administrator
+ */
 @Configuration
 //报错解决方法的注解方法一
 @EnableConfigurationProperties(MinioProperties.class)
